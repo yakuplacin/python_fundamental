3996dd31b67156d248e86607fa3c260d76c70894
Yakup LACIN <lightsuksesyon@gmail.com>
2023-03-29 23:34:51 +0300

    quiz

--- OOP/demo-quiz.py
+++ OOP/demo-quiz.py
@@ -28,16 +28,50 @@ class Question:
 
 class Quiz:
     def __init__(self, questions):
-        self.qustions = questions
+        self.questions = questions
         self.score = 0
         self.questionIndex = 0
 
     def getQuestion(self):
-        return self.qustions[self.questionIndex]
+        return self.questions[self.questionIndex]
 
     def displayQuestion(self):
         question = self.getQuestion()
         print(f"Question {self.questionIndex + 1}: {question.text}")
+
+        for q in question.choices:
+            print('-' + q)
+
+        answer = input("Answer: ")
+        self.guess(answer)
+        self.loadQuestion()
+
+    def guess(self, answer):
+        question = self.getQuestion()
+
+        if question.checkAnswer(answer):
+            self.score += 1
+
+        self.questionIndex += 1
+
+    def loadQuestion(self):
+        if len(self.questions) == self.questionIndex:
+            self.showScore()
+        else:
+            self.displayProgress()
+            self.displayQuestion()
+
+    def showScore(self):
+        print(f"score: {self.score}")
+
+    def displayProgress(self):
+        totalQuestion = len(self.questions)
+        questionNumber = self.questionIndex + 1
+
+        if questionNumber > totalQuestion:
+            print("Quiz is finished")
+        else:
+            print(f"Question {questionNumber} of {totalQuestion}")
 
 
 q1 = Question("Which program is more useful for data science?",
@@ -56,4 +90,6 @@ quiz = Quiz(questions)
 question = quiz.getQuestion()
 # print(question.text)
 
-quiz.displayQuestion()
+# quiz.displayQuestion()
+
+quiz.loadQuestion()
