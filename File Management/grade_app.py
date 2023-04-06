817764643d322ad4e2001a84e81d110d34b34453
Yakup LACIN <lightsuksesyon@gmail.com>
2023-04-07 01:19:38 +0300

    file management

--- File Management/grade_app.py
+++ File Management/grade_app.py
@@ -2,7 +2,42 @@ def calculate_grade(line):
     line = line[:-1]
     grList = line.split(":")
     studentName = grList[0]
-    grades =
+    grades = grList[1].split(",")
+
+    grades1 = int(grades[0])
+    grades2 = int(grades[1])
+    grades3 = int(grades[2])
+
+    print(grades1)
+    print(grades2)
+    print(grades3)
+
+    average = (grades1 + grades2 + grades3)/3
+
+    if average >= 90 and average <= 100:
+        grade = "A"
+    elif average >= 87:
+        grade = "A-"
+    elif average >= 83:
+        grade = "B+"
+    elif average >= 80:
+        grade = "B"
+    elif average >= 77:
+        grade = "B-"
+    elif average >= 73:
+        grade = "C+"
+    elif average >= 70:
+        grade = "C"
+    elif average >= 64:
+        grade = "C-"
+    elif average >= 56:
+        grade = "D+"
+    elif average >= 50:
+        grade = "D"
+    elif average >= 0 and average <= 50:
+        grade = "F"
+
+    return studentName + ": " + grade + "\n"
 
 
 def read_average():
@@ -24,7 +59,15 @@ def give_grade():
 
 
 def save_grade():
-    pass
+    with open("exam_grades.txt", "r", encoding="utf-8") as file:
+        gradeList = []
+
+        for i in file:
+            gradeList.append(calculate_grade(i))
+
+        with open("results.txt", "w", encoding="utf-8") as file2:
+            for i in gradeList:
+                file2.write(i)
 
 
 while True:
